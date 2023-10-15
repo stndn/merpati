cd817937991ce23dae6a15855d395a5883ceeb9a
Sutan <stndn@users.noreply.github.com>
2023-10-16 00:03:51 +0800

    Added supporting files for processing weather data files in S3

---
+++ weather-tl/config/app_config.py
@@ -0,0 +1,21 @@
+# Application configuration
+
+import os
+from time import localtime, strftime
+
+class Config(object):
+  DEBUG = False
+  TESTING = False
+  app_version = "v1"
+  APP_PATH = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir))
+
+class Development(Config):
+  DEBUG = True
+
+class Production(Config):
+  DEBUG = False
+
+class Testing(Config):
+  DEBUG = False
+  TESTING=True
+
